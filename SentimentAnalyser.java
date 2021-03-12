c129d6bb1cbb547a60075a48ea9cb05140c1cb7e
Aidan-Filby <52698564+Aidan-Filby@users.noreply.github.com>
2021-03-12 16:09:03 +0000

    Update SentimentAnalyser.java

--- SentimentAnalyser.java
+++ SentimentAnalyser.java
@@ -8,38 +8,55 @@ import edu.stanford.nlp.coref.data.CorefChain;
 import edu.stanford.nlp.io.*;
 import edu.stanford.nlp.ling.*;
 import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
 import edu.stanford.nlp.semgraph.SemanticGraphEdge;
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+// import edu.stanford.nlp.sentiment.SentimentAnnotater;
 import edu.stanford.nlp.trees.*;
 import edu.stanford.nlp.util.*;
 
-// Sentiment Analyser Object creates one CoreNLP pipeline, and then has one method to take in textual feedback and return a value for sentiment
+
 public class SentimentAnalyser {
 
-  public Properties props = new Properties();     // sentiment analyser object has properties
-  public StanfordCoreNLP pipeline;                // and a pipeline used for annotations
+  public Properties props = new Properties();
+  public StanfordCoreNLP pipeline;
 
   public SentimentAnalyser(){
-    props.setProperty("annotators", "tokenize, ssplit, pos, parse, sentiment");   // properties needed to provide sentiment analysis
-    pipeline = new StanfordCoreNLP(props);                                        // creates pipeline object used for analysis
+    props.setProperty("annotators", "tokenize, ssplit, pos, parse, sentiment");
+    pipeline = new StanfordCoreNLP(props);
   }
-    // takes in text, gives an integer value (0-4) representing sentiment for each sentence, and then returns the average
+
   public float Analyse(String text){
     float totalSentiment = 0;
     float sum = 0;
-    Annotation annotation = pipeline.process(line);
+    Annotation annotation = pipeline.process(text);
     for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
-      Tree tree = sentence.get(SentimentCoreAnnotations.AnnotatedTree.class);
-      totalSentiment += RNNCoreAnnotations.getPredictedClass(tree);
+      String mood = sentence.get(SentimentCoreAnnotations.SentimentClass.class);
+      // if (mood.equals("Very negative")){
+      //   totalSentiment+=0
+      // }
+      if (mood.equals("Negative")){
+        totalSentiment+=1;
+      }
+      if (mood.equals("Neutral")){
+        totalSentiment+=2;
+      }
+      if (mood.equals("Positive")){
+        totalSentiment+=3;
+      }
+      if (mood.equals("Very positive")){
+        totalSentiment+=4;
+      }
+      // totalSentiment += RNNCoreAnnotations.getPredictedClass(tree);
       sum += 1;
     }
     if (sum == 0){
-      return 2.0;
+      return 2;
     }
     return (totalSentiment/sum);
   }
 
-    
+
 }
